c100bcd48a47dd2f798e14f737250b43ce8248d7
booziyt1-code <aboubakrkour2010@gmail.com>
2026-06-23 11:07:04 +0100

    Add Hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,21 @@ export default function RootLayout({
     `,
   }}
 />
-
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:6713346,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');
+          r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `,
+  }}
+/>
   </head>
 
 
