d6e84b9c48d5b7e310dc387aa90c4b12adc63d90
booziyt1-code <aboubakrkour2010@gmail.com>
2026-07-22 11:54:02 +0100

    Update

--- data/videos.ts
+++ data/videos.ts
@@ -1,22 +1,22 @@
 export const videos = [
   {
     title: "رحلة إنتاج زيت الزيتون الطبيعي",
-    thumbnail: "/videos/olive.jpg",
+    thumbnail: "/videos/olive.webp",
     url: "https://www.facebook.com/share/v/1Myvztvnvr/",
   },
   {
     title: "استخراج العسل من مناحلنا",
-    thumbnail: "/videos/honey.jpg",
+    thumbnail: "/videos/honey.webp",
     url: "https://www.facebook.com/share/r/18XRrL77AJ/",
   },
   {
     title: "اكتشف عرض الأركان لهذا الصيف",
-    thumbnail: "/videos/argan.jpg",
+    thumbnail: "/videos/argan.webp",
     url: "https://www.facebook.com/share/r/1M71g3rytf/",
   },
   {
     title: "تعرف على باقة الصيف المميزة",
-    thumbnail: "/videos/summer.jpg",
+    thumbnail: "/videos/summer.webp",
     url: "https://www.facebook.com/share/r/1ao9PaFrnu/",
   },
 ];
