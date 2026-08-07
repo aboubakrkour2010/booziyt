e1b6af365f3ca9c481a48f85586629812543419d
booziyt1-code <aboubakrkour2010@gmail.com>
2026-08-07 21:48:49 +0100

    Update

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,9 +1,25 @@
 import Hero from "./components/Hero";
+import ProductDescription from "./components/ProductDescription";
+import BundleContents from "./components/BundleContents";
+import PriceSummary from "./components/PriceSummary";
+import Benefits from "./components/Benefits";
+import Reviews from "./components/Reviews";
+import FAQ from "./components/FAQ";
+import OrderForm from "./components/OrderForm";
+import StickyOrder from "./components/StickyOrder";
 
 export default function LandingPage() {
   return (
     <main>
       <Hero />
+      <ProductDescription />
+      <BundleContents />
+      <PriceSummary />
+      <Benefits />
+      <Reviews />
+      <FAQ />
+      <OrderForm />
+      <StickyOrder />
     </main>
   );
 }
